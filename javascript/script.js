ece355cdd73934999d1d6fd9585ac9878f08e2fd
Kimondorose <rosewairimu1@gmail.com>
2020-07-27 02:19:45 +0300

    add button function

--- javascript/script.js
+++ javascript/script.js
@@ -2,3 +2,10 @@ $('.main').click(function() {
     $(this).children ('.hide').slideToggle(1200);
     $(this).children ('.show').toggle(1500);
 });
+
+$("button").click (function (omonge){
+    var client = document.getElementById('nameDetail').value;
+    alert('Dear ' + client + ','+ 'we have received your message. ' + 'Thank you for reaching out to us. ' +"We will get back to you as soon as possible");
+    omonge.preventDefault();
+});
+
