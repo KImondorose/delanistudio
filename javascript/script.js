c682b8c293c691d596af92f2d61b4f1ae34256ed
Kimondorose <rosewairimu1@gmail.com>
2020-07-27 03:09:48 +0300

    add readme

--- javascript/script.js
+++ javascript/script.js
@@ -1,6 +1,6 @@
 $('.main').click(function() {
-    $(this).children ('.hide').slideToggle(1200);
-    $(this).children ('.show').toggle(1500);
+    $(this).children ('.show').slideToggle(1200);
+    $(this).children ('.hide').toggle(1500);
 });
 
 $("button").click (function (omonge){
